7458a4f2d8d074322b46d294b0c06fb4b6e810b8
chiibu21 <chiibu21@users.noreply.github.com>
2018-07-04 09:13:32 -0400

    remove notifications

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -14,16 +14,8 @@ export class HeaderComponent implements OnInit {
     this.auth.user$.subscribe(data => this.user = data);
   }
   messages = [
-    {title:'Hi this is Clara!',message:"I have sent the lesson notes to all students. Remember to call principle Sheperd for more directions. Thank you!"},
-    {title:"Student loans due",message:"No loans will be repayed and you have lost all your money. I lack creativity. Lmao!"},
-    {title:"Who let the dogs out?",message:"Sandra did. She sold the leashes to some dude from 44th Street."}
   ];
   notifications = [
-    {title:"Adams asked to join jupita leage coding group",more:""},
-    {title:"Adams asked to join jupita leage coding group",more:""},
-    {title:"Adams asked to join jupita leage coding group",more:""},
-    {title:"Adams asked to join jupita leage coding group",more:""},
-    {title:"Adams asked to join jupita leage coding group",more:""}
   ];
   popItem(not){
     this.notifications.splice(this.notifications.indexOf(not),1);
