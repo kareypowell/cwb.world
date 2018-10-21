7716088f702498f7bf989b8ae329114899cd8e18
chiibu21 <chiibu21@users.noreply.github.com>
2018-10-21 00:43:15 -0400

    test keys added

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  stripeKey: 'pk_test_HFeR2lSgAkLBWCcl9L1wSR7M',
+  firebaseConfig :{
+    apiKey: "AIzaSyCEVwNPKz4Ae23-Jonn51ni8U--W3c1hEk",
+    authDomain: "cwb-world.firebaseapp.com",
+    databaseURL: "https://cwb-world.firebaseio.com",
+    projectId: "cwb-world",
+    storageBucket: "cwb-world.appspot.com",
+    messagingSenderId: "84926444510"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ frontend/src/environments/environments.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    stripeKey: 'pk_test_HFeR2lSgAkLBWCcl9L1wSR7M',
+    firebaseConfig :{
+      apiKey: "AIzaSyCEVwNPKz4Ae23-Jonn51ni8U--W3c1hEk",
+      authDomain: "cwb-world.firebaseapp.com",
+      databaseURL: "https://cwb-world.firebaseio.com",
+      projectId: "cwb-world",
+      storageBucket: "cwb-world.appspot.com",
+      messagingSenderId: "84926444510"
+    }
+  };
+  
