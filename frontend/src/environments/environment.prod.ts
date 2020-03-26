4a5bdc69789534b60ea2a17d7608f379a111df20
Karey Powell <kareypowell@gmail.com>
2020-03-26 11:28:44 -0500

    Update project dependencies, separate envs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,17 @@
+// environment.prod.ts
+// configurations for production firebase projects
+
 export const environment = {
   production: true,
   stripeKey: 'pk_test_HFeR2lSgAkLBWCcl9L1wSR7M',
   firebaseConfig: {
-    apiKey: "AIzaSyDNvKedHqWj_b3zKlMy3z6DYJ0yW6Q0usM",
-    authDomain: "fir-app-220117.firebaseapp.com",
-    databaseURL: "https://fir-app-220117.firebaseio.com",
-    projectId: "firebaseapp-220117",
-    storageBucket: "firebaseapp-220117.appspot.com",
-    messagingSenderId: "429387056804"
+    apiKey: "AIzaSyCXcF2CkYtT7ktrwKmy0XkY84azyDRRCMg",
+    authDomain: "cwb-production-272314.firebaseapp.com",
+    databaseURL: "https://cwb-production-272314.firebaseio.com",
+    projectId: "cwb-production-272314",
+    storageBucket: "cwb-production-272314.appspot.com",
+    messagingSenderId: "570207075108",
+    appId: "1:570207075108:web:d893f67fc3c3417223d1b1",
+    measurementId: "G-QKWDMYEFG2"
   }
 };
