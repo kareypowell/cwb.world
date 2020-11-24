c00095d4540e401607a1c0293fc954550f838b5e
ANKIT <ankit@visions.net.in>
2020-11-24 18:34:46 +0530

    update firebase

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import { AuthService } from './auth-service';
 import { Router, NavigationEnd } from '@angular/router';
 
